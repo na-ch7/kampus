89c9ccce2f5c8e14b6e83f0c98b02d02d726aeab
na-ch7 <141823845+na-ch7@users.noreply.github.com>
2024-10-20 03:46:55 +0530

    setup next.js

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'kampus',
+  description: 'Oneplace stop to explore all campus events',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+    
+    </html>
+  )
+}
+
